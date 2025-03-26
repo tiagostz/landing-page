bed7a09a792029114cc885ac57bc801b36314461
tiagostz <tiagosantos.01016@gmail.com>
2025-03-26 19:09:12 -0300

    criacao do formulario

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+
+@Component({
+  selector: 'app-newsletter-form',
+  standalone: true,
+  imports: [
+    BtnPrimaryComponent
+  ],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss'
+})
+export class NewsletterFormComponent {
+
+}
